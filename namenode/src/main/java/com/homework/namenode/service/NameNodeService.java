200e6029615266e08266e956c4c03fbd60393ea6
蒋雨霖 <30835144+jimmy233@users.noreply.github.com>
2018-06-30 19:55:17 +0800

    Update NameNodeService.java

--- namenode/src/main/java/com/homework/namenode/service/NameNodeService.java
+++ namenode/src/main/java/com/homework/namenode/service/NameNodeService.java
@@ -5,11 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-/**
- * @author sunyiran
- * @date 2018-06-29
- * @purpose
- */
+
 public interface NameNodeService {
     String upload(MultipartFile file);
     void delete(String filename);
